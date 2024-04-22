c188e68fdf4b0a4632267bf8f2ec52fbff9ac803
Lillian Rose <opusbopus@proton.me>
2024-04-21 23:19:14 -0400

    Initial commit

---
+++ Crypt.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GunfireCrypt;
+
+public class Crypt : IDisposable
+{
+    private readonly Rijndael crypt = Rijndael.Create();
+    private readonly ICryptoTransform decryptor;
+    private readonly ICryptoTransform encryptor;
+    private readonly byte[] key = Encoding.UTF8.GetBytes("jwnnAS92nfe9Sas92nrSDdsn83Sd2w8".PadRight(32));
+    private readonly byte[] vector = "ai9d9vjnSNSd8f3w"u8.ToArray();
+
+    public Crypt()
+    {
+        encryptor = crypt.CreateEncryptor(key, vector);
+        decryptor = crypt.CreateDecryptor(key, vector);
+    }
+
+    public void Dispose()
+    {
+        crypt.Dispose();
+    }
+
+    public byte[] encrypt(byte[] data)
+    {
+        using var stream = new MemoryStream(data);
+        using var cryptoStream = new CryptoStream(stream, encryptor, CryptoStreamMode.Read);
+        using var outStream = new MemoryStream();
+        cryptoStream.CopyTo(outStream);
+        return outStream.ToArray();
+    }
+
+    public byte[] decrypt(byte[] data)
+    {
+        using var stream = new MemoryStream(data);
+        using var cryptoStream = new CryptoStream(stream, decryptor, CryptoStreamMode.Read);
+        using var outStream = new MemoryStream();
+        cryptoStream.CopyTo(outStream);
+        return outStream.ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using GunfireCrypt;
+
+#pragma warning disable SYSLIB0022
+
+
+var saveData = File.ReadAllBytes("C:\\Users\\lily\\Documents\\user_1.dat");
+Console.WriteLine($"Save file has {saveData.Length} bytes");
+var header = saveData[..4];
+if (!header.SequenceEqual("EF2S"u8.ToArray()))
+{
+    Console.WriteLine("Bad header. Expected EF2S");
+    return;
+}
+
+Console.WriteLine("Good header");
+
+var crc16Hash = BitConverter.ToUInt16(saveData, header.Length + 1);
+Console.WriteLine($"Hash = {crc16Hash}");
+
+var dataToDecrypt = saveData[(header.Length + 3)..];
+using var crypt = new Crypt();
+
+var decryptedBytes = crypt.decrypt(dataToDecrypt);
+var encryptedBytes = crypt.encrypt(decryptedBytes);
+""
+Console.WriteLine($"{dataToDecrypt.SequenceEqual(encryptedBytes)}");
+
+var hash = Crc16.ComputeChecksum(encryptedBytes);
+Console.WriteLine($"New hash = {hash}");
+
+using var outputStream = new MemoryStream();
+outputStream.Write("EF2S\x1"u8.ToArray());
+outputStream.Write(BitConverter.GetBytes(hash));
+outputStream.Write(encryptedBytes);
+
+File.WriteAllBytes("C:\\Users\\lily\\Documents\\user_3.dat", outputStream.ToArray());
+
+
+Console.WriteLine("Hello, World!");
